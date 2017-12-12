fbe3c498eba598eb8caf43c5f9addfd9e3e9261f
yursile <kuangyu02@meituan.com>
2017-12-12 18:04:02 +0800

    v1

---
+++ store/index.js
@@ -0,0 +1,26 @@
+// import Vuex from 'vuex'
+
+// const createStore = () => {
+//   return new Vuex.Store({
+//     state: {
+//       counter: 0
+//     },
+//     mutations: {
+//       increment (state) {
+//         state.counter++
+//       }
+//     }
+//   })
+// }
+
+export const state = () => ({
+    counter: 0
+})
+  
+export const mutations = {
+    increment (state) {
+        state.counter++
+    }
+}
+
+// export default createStore
